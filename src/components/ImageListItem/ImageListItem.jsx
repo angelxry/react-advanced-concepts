6a61e74dcd79c71daa73cd287dac8929919502fd
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-02-15 19:18:04 +0100

    wip

--- src/components/ImageListItem/ImageListItem.jsx
+++ src/components/ImageListItem/ImageListItem.jsx
@@ -9,10 +9,10 @@ export function ImageListItem({ img }) {
   return (
     <div className="card">
       <a href={img.url}>
-        <img src={img.download_url} className="img" />
+        <img src={img.download_url} className="img" alt="" />
       </a>
       <div className="card_banner">
-        <img src={img.download_url} className="card_thumb" />
+        <img src={img.download_url} className="card_thumb" alt="" />
         <div className="card_text">
           <h3 className="card_title">{img.author}</h3>
           <div className="card_status">
