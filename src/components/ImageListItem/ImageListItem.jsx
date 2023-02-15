af8d770e93b4524c662c422f03fe4ce88a8d21b9
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-02-15 15:37:02 +0100

    wip

--- src/components/ImageListItem/ImageListItem.jsx
+++ src/components/ImageListItem/ImageListItem.jsx
@@ -1,20 +1,4 @@
 import "./ImageListItem.css";
 export function ImageListItem({ img }) {
-  return (
-    <div className="card">
-      <a href={img.url}>
-        <img src={img.download_url} className="img" />
-      </a>
-      <div className="card_banner">
-        <img src={img.download_url} className="card_thumb" />
-        <div className="card_text">
-          <h3 className="card_title">{img.author}</h3>
-          <div className="card_status">
-            Original size: {img.height}x{img.width}
-          </div>
-          <button>Download</button>
-        </div>
-      </div>
-    </div>
-  );
+  return <img src={img.download_url} className="img" />;
 }
